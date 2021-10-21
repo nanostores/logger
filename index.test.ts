e9caf4d53b451655cc69b7d865131eef8f69ee98
Eddort <webisforce@gmail.com>
2021-10-21 16:30:42 +0300

    wip

--- index.test.ts
+++ index.test.ts
@@ -1,12 +1,22 @@
 /* eslint-disable no-console */
-import { action, atom } from 'nanostores'
+import { action, atom, map, mapTemplate } from 'nanostores'
 import { jest } from '@jest/globals'
 import { SpyInstance } from 'jest-mock'
 import { delay } from 'nanodelay'
 
 import { logger } from './index.js'
 
-describe('Nanostores Logger', () => {
+let atomStore = atom({ value: 0 })
+let mapStore = map({ value: 0, id: '1' })
+let TemplateStore = mapTemplate<{ value: number }>(s => {
+  s.set({ value: 0, id: '1' })
+})
+
+describe.each([
+  [atomStore, 'atomStore'],
+  [mapStore, 'mapStore'],
+  [TemplateStore('1'), 'TemplateStore-1']
+])('Base log types', (store, storeName) => {
   let consoleGroupCollapsedMock: SpyInstance<void, any[]>
   let consoleLogMock: SpyInstance<
     void,
@@ -14,7 +24,6 @@ describe('Nanostores Logger', () => {
   >
   let consoleGroupEndMock: SpyInstance<void, any[]>
 
-  let atomStore = atom(0)
   beforeEach(() => {
     consoleGroupCollapsedMock = jest
       .spyOn(console, 'groupCollapsed')
@@ -30,18 +39,18 @@ describe('Nanostores Logger', () => {
     consoleGroupEndMock.mockRestore()
   })
 
-  it('Atom: connected', () => {
-    let unbind = logger({ atomStore })
+  it(`${storeName}: connected`, () => {
+    let unbind = logger({ [storeName]: store })
 
     expect(console.groupCollapsed).toHaveBeenLastCalledWith(
-      '%cnanostores%cstart%catomStore',
+      `%cnanostores%cstart%c${storeName}`,
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:blue;',
       'padding-left:4px;padding-right:4px;font-weight:normal;'
     )
 
     expect(console.log).toHaveBeenLastCalledWith(
-      '%cLogger was connected to atomStore',
+      `%cLogger was connected to ${storeName}`,
       'padding-left:4px;padding-right:4px;font-weight:normal;'
     )
 
@@ -52,13 +61,12 @@ describe('Nanostores Logger', () => {
     unbind()
   })
 
-  it('Atom: mount/unmount', async () => {
-    // expect(console.warn.mock.calls)
-    let unbind = logger({ atomStore })
-    let unbindStore = atomStore.listen(() => {})
+  it(`${storeName}: mount/unmount`, async () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
 
     expect(console.groupCollapsed).toHaveBeenLastCalledWith(
-      '%cnanostores%ccreate%catomStore',
+      `%cnanostores%ccreate%c${storeName}`,
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:#8f1fff;',
       'padding-left:4px;padding-right:4px;font-weight:normal;'
@@ -74,7 +82,7 @@ describe('Nanostores Logger', () => {
     await delay(1020)
 
     expect(console.groupCollapsed).toHaveBeenLastCalledWith(
-      '%cnanostores%cstop%catomStore',
+      `%cnanostores%cstop%c${storeName}`,
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
       'padding-left:4px;padding-right:4px;font-weight:normal;'
@@ -91,127 +99,369 @@ describe('Nanostores Logger', () => {
 
     unbind()
   })
+})
 
-  it('Atom: change', () => {
-    let unbind = logger({ atomStore })
-    let unbindStore = atomStore.listen(() => {})
-    atomStore.set(2)
+describe('Atom changes', () => {
+  let [store, storeName] = [atomStore, 'atomStore']
+  let consoleGroupCollapsedMock: SpyInstance<void, any[]>
+  let consoleLogMock: SpyInstance<
+    void,
+    [message?: any, ...optionalParams: any[]]
+  >
+  let consoleGroupEndMock: SpyInstance<void, any[]>
+
+  beforeEach(() => {
+    consoleGroupCollapsedMock = jest
+      .spyOn(console, 'groupCollapsed')
+      .mockImplementation(() => {})
+    consoleLogMock = jest.spyOn(console, 'log').mockImplementation(() => {})
+    consoleGroupEndMock = jest
+      .spyOn(console, 'groupEnd')
+      .mockImplementation(() => {})
+  })
+  afterEach(() => {
+    consoleGroupCollapsedMock.mockRestore()
+    consoleLogMock.mockRestore()
+    consoleGroupEndMock.mockRestore()
+  })
+  it(`${storeName}: change`, () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
+    store.set({ value: 2 })
 
     expect(console.groupCollapsed).toHaveBeenLastCalledWith(
-      '%cnanostores%cchange%catomStore →',
+      `%cnanostores%cchange%c${storeName} →`,
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
       'padding-left:4px;padding-right:4px;font-weight:normal;',
-      2
+      { value: 2 }
     )
 
-    expect(consoleLogMock.mock.calls).toMatchInlineSnapshot(`
-      Array [
-        Array [
-          "%cLogger was connected to atomStore",
-          "padding-left:4px;padding-right:4px;font-weight:normal;",
-        ],
-        Array [
-          "%cStore was mounted",
-          "padding-left:4px;padding-right:4px;font-weight:normal;",
-        ],
-        Array [
-          "%cnew",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;",
-          2,
-        ],
+    expect(consoleLogMock.mock.calls).toEqual([
+      [
+        `%cLogger was connected to ${storeName}`,
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%cStore was mounted',
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 2 }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 0 }
       ]
-    `)
+    ])
 
     unbindStore()
     unbind()
   })
 
-  it('Atom: change with old', () => {
-    let unbind = logger({ atomStore })
-    let unbindStore = atomStore.listen(() => {})
-    atomStore.set(2)
-    atomStore.set(3)
+  it(`${storeName}: change with old`, () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
+    store.set({ value: 2 })
+    store.set({ value: 3 })
 
     expect(console.groupCollapsed).toHaveBeenLastCalledWith(
-      '%cnanostores%cchange%catomStore →',
+      `%cnanostores%cchange%c${storeName} →`,
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
       'padding-left:4px;padding-right:4px;font-weight:normal;',
-      3
+      { value: 3 }
     )
 
-    expect(consoleLogMock.mock.calls).toMatchInlineSnapshot(`
-      Array [
-        Array [
-          "%cLogger was connected to atomStore",
-          "padding-left:4px;padding-right:4px;font-weight:normal;",
-        ],
-        Array [
-          "%cnew",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;",
-          2,
-        ],
-        Array [
-          "%cold",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;",
-          2,
-        ],
-        Array [
-          "%cnew",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;",
-          3,
-        ],
-        Array [
-          "%cold",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;",
-          2,
-        ],
+    expect(consoleLogMock.mock.calls).toEqual([
+      [
+        '%cLogger was connected to atomStore',
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 2 }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 2 }
+      ],
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 3 }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 2 }
       ]
-    `)
+    ])
 
     unbindStore()
     unbind()
   })
 
-  it('Atom: change with action', () => {
-    let unbind = logger({ atomStore })
-    let unbindStore = atomStore.listen(() => {})
-    let myAction = action(atomStore, 'myAction', store => {
-      store.set(44)
+  it(`${storeName}: change with action`, () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
+    let myAction = action(store, 'myAction', s => {
+      s.set({ value: 44 })
     })
     myAction()
     expect(console.groupCollapsed).toHaveBeenLastCalledWith(
-      '%cnanostores%cchange%catomStore →',
+      `%cnanostores%cchange%c${storeName} →`,
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
       'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
       'padding-left:4px;padding-right:4px;font-weight:normal;',
-      44
+      { value: 44 }
     )
 
-    expect(consoleLogMock.mock.calls).toMatchInlineSnapshot(`
-      Array [
-        Array [
-          "%cLogger was connected to atomStore",
-          "padding-left:4px;padding-right:4px;font-weight:normal;",
-        ],
-        Array [
-          "%caction",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:indigo;",
-          "myAction",
-        ],
-        Array [
-          "%cnew",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;",
-          44,
-        ],
-        Array [
-          "%cold",
-          "color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;",
-          3,
-        ],
+    expect(consoleLogMock.mock.calls).toEqual([
+      [
+        '%cLogger was connected to atomStore',
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%caction',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:indigo;',
+        'myAction'
+      ],
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 44 }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 3 }
       ]
-    `)
+    ])
+
+    unbindStore()
+    unbind()
+  })
+})
+
+describe.each([
+  [mapStore, 'mapStore'],
+  [
+    (() => {
+      let TemplateStore2 = mapTemplate<{ value: number }>(s => {
+        s.set({ value: 0, id: '1' })
+      })
+      let store = TemplateStore2('1')
+      //@ts-ignore
+      store.value = { ...store.value, value: 0 }
+      return store
+    })(),
+    'TemplateStore-1'
+  ]
+])('Map changes', (store, storeName) => {
+  let consoleGroupCollapsedMock: SpyInstance<void, any[]>
+  let consoleLogMock: SpyInstance<
+    void,
+    [message?: any, ...optionalParams: any[]]
+  >
+  let consoleGroupEndMock: SpyInstance<void, any[]>
+
+  beforeEach(() => {
+    consoleGroupCollapsedMock = jest
+      .spyOn(console, 'groupCollapsed')
+      .mockImplementation(() => {})
+    consoleLogMock = jest.spyOn(console, 'log').mockImplementation(() => {})
+    consoleGroupEndMock = jest
+      .spyOn(console, 'groupEnd')
+      .mockImplementation(() => {})
+  })
+  afterEach(() => {
+    consoleGroupCollapsedMock.mockRestore()
+    consoleLogMock.mockRestore()
+    consoleGroupEndMock.mockRestore()
+  })
+  it(`${storeName}: change`, () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
+    store.set({ value: 2, id: '1' })
+
+    expect(console.groupCollapsed).toHaveBeenLastCalledWith(
+      `%cnanostores%cchange%c${storeName} →`,
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+      'padding-left:4px;padding-right:4px;font-weight:normal;',
+      { value: 2, id: '1' }
+    )
+
+    expect(consoleLogMock.mock.calls).toEqual([
+      [
+        `%cLogger was connected to ${storeName}`,
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%cStore was mounted',
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 2, id: '1' }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 0, id: '1' }
+      ]
+    ])
+
+    unbindStore()
+    unbind()
+  })
+
+  it(`${storeName}: change with old`, () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
+    store.set({ value: 2, id: '1' })
+    store.set({ value: 3, id: '1' })
+
+    expect(console.groupCollapsed).toHaveBeenLastCalledWith(
+      `%cnanostores%cchange%c${storeName} →`,
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+      'padding-left:4px;padding-right:4px;font-weight:normal;',
+      { value: 3, id: '1' }
+    )
+
+    expect(consoleLogMock.mock.calls).toEqual([
+      [
+        `%cLogger was connected to ${storeName}`,
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 2, id: '1' }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 2, id: '1' }
+      ],
+
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 3, id: '1' }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 2, id: '1' }
+      ]
+    ])
+
+    unbindStore()
+    unbind()
+  })
+
+  it(`${storeName}: change with action`, () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
+    let myAction = action(store, 'myAction', s => {
+      s.set({ id: '1', value: 44 })
+    })
+    myAction()
+    expect(console.groupCollapsed).toHaveBeenLastCalledWith(
+      `%cnanostores%cchange%c${storeName} →`,
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+      'padding-left:4px;padding-right:4px;font-weight:normal;',
+      { id: '1', value: 44 }
+    )
+
+    expect(consoleLogMock.mock.calls).toEqual([
+      [
+        `%cLogger was connected to ${storeName}`,
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%caction',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:indigo;',
+        'myAction'
+      ],
+
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { id: '1', value: 44 }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { id: '1', value: 3 }
+      ]
+    ])
+
+    unbindStore()
+    unbind()
+  })
+
+  it(`${storeName}: setKey`, () => {
+    let unbind = logger({ [storeName]: store })
+    let unbindStore = store.listen(() => {})
+    store.setKey('value', 2)
+    store.setKey('value', 3)
+
+    expect(console.groupCollapsed).toHaveBeenLastCalledWith(
+      `%cnanostores%cchange%c${storeName} →`,
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:black;',
+      'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+      'padding-left:4px;padding-right:4px;font-weight:normal;',
+      { value: 3, id: '1' }
+    )
+
+    expect(consoleLogMock.mock.calls).toEqual([
+      [
+        `%cLogger was connected to ${storeName}`,
+        'padding-left:4px;padding-right:4px;font-weight:normal;'
+      ],
+      [
+        '%cchanged',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:MidnightBlue;',
+        'value'
+      ],
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 2, id: '1' }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 44, id: '1' }
+      ],
+      [
+        '%cchanged',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:MidnightBlue;',
+        'value'
+      ],
+      [
+        '%cnew',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:green;',
+        { value: 3, id: '1' }
+      ],
+      [
+        '%cold',
+        'color:white;padding-left:4px;padding-right:4px;font-weight:normal;background:tomato;',
+        { value: 2, id: '1' }
+      ]
+    ])
 
     unbindStore()
     unbind()
