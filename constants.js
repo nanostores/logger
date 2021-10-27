10dc18b9eea87e93bc1c526d1b99f5ee136773ca
Eduard Aksamitov <e@edaks.co>
2021-10-27 18:10:05 +0300

    feat: new colors

--- constants.js
+++ constants.js
@@ -6,10 +6,10 @@ let printStyles = (background, halfRadius = false) =>
 export let styles = {
   badge: ({ badge }) =>
     `background-image: url("${badge}");background-size: cover;color:white;padding: 0 3.5px;margin-right: 4px`,
-  new: printStyles('#1da1f2', true),
-  old: printStyles('#1da1f2', true),
-  action: printStyles('indigo', true),
-  changed: printStyles('MidnightBlue', true),
+  new: printStyles('#4FA574', true),
+  old: printStyles('#4FA574', true),
+  action: printStyles('#5351A4', true),
+  changed: printStyles('#429BD7', true),
   message:
     'border-radius:10px;font-family:Menlo,monospace;margin-left:4px;padding:0 5px;',
   storeName:
@@ -17,8 +17,8 @@ export let styles = {
 }
 
 export let logTypesStyles = {
-  mount: printStyles('blue'),
-  build: printStyles('#8f1fff'),
-  change: printStyles('green'),
-  unmount: printStyles('tomato')
+  mount: printStyles('#1F49E0'),
+  build: printStyles('#BB5100'),
+  change: printStyles('#0E8A00'),
+  unmount: printStyles('#C74777')
 }
