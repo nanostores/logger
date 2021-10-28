f67945fba4baa67301f7a0bcc3f60ee7b16a077b
Eduard Aksamitov <e@edaks.co>
2021-10-29 02:46:43 +0300

    fix: missed `MapTemplate` logs

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
-import { lastAction, onBuild, onMount, onSet, onStop } from 'nanostores'
+import {
+  STORE_UNMOUNT_DELAY,
+  lastAction,
+  onBuild,
+  onStart,
+  onStop,
+  onSet
+} from 'nanostores'
 
 import { group, log, nested } from './printer.js'
 
 let handleSet = (storeName, store) =>
   onSet(store, ({ changed, newValue }) => {
     let actionName = store[lastAction]
-    let message = [['text', 'was changed in key'], ['bold', changed]]
+    let message = [
+      ['text', 'was changed in key'],
+      ['bold', changed]
+    ]
     if (actionName) {
       message.push(['text', 'by'], ['bold', actionName], ['text', 'action'])
     }
-    group(
-      { logType: 'change', storeName, message },
-      () => {
-        nested({
-          actionName,
-          changed,
-          newValue,
-          oldValue: store.get(),
-          logType: 'change'
-        })
-      }
-    )
+    group({ logType: 'change', storeName, message }, () => {
+      nested({
+        actionName,
+        changed,
+        newValue,
+        oldValue: store.get(),
+        logType: 'change'
+      })
+    })
   })
 
-let handleMount = (storeName, store) =>
-  onMount(store, () => {
+let handleMount = (storeName, store) => {
+  let unbindStart = onStart(store, () => {
     log({
       logType: 'mount',
       storeName,
       message: 'was mounted'
     })
-    return () => {
+  })
+  let unbindStop = onStop(store, () => {
+    setTimeout(() => {
       log({
         logType: 'unmount',
         storeName,
         message: 'was unmounted'
       })
-    }
+    }, STORE_UNMOUNT_DELAY)
   })
+  return () => {
+    unbindStart()
+    unbindStop()
+  }
+}
 
 let storeLogger = (storeName, store) => {
   let unsubs = [handleSet(storeName, store), handleMount(storeName, store)]
@@ -50,12 +64,17 @@ let templateLogger = (templateName, template, nameGetter) =>
     log({
       logType: 'build',
       storeName,
-      message: [['text', 'was built by'], ['bold', templateName]]
+      message: [
+        ['text', 'was built by'],
+        ['bold', templateName]
+      ]
     })
     let unsubLog = storeLogger(storeName, store)
     let usubStop = onStop(store, () => {
-      unsubLog()
-      usubStop()
+      setTimeout(() => {
+        unsubLog()
+        usubStop()
+      }, STORE_UNMOUNT_DELAY + 1)
     })
   })
 
