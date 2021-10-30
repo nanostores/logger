150075c043349881f8dc5b52997bc57d908b549e
Eduard Aksamitov <e@edaks.co>
2021-10-30 03:24:41 +0300

    fix: reduce `mount`/`unmount` events

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ let handle = ([storeName, store], nameGetter) =>
     : storeLogger(storeName, store)
 
 let defaultNameGetter = (store, templateName) =>
-  `${templateName}-${store.get().id}`
+  `${templateName}-${store.value.id}`
 
 export let logger = (deps, opts = {}) => {
   deps = Object.entries(deps)
