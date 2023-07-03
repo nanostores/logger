3c264f4a3ff6ba11d95df0fc5f9c61b6dc250798
Eduard Aksamitov <e@euaaaio.ru>
2023-07-04 01:04:58 +0300

    chore: export `buildLogger`

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+export { buildLogger } from './build-logger/index.js'
 export { creatorLogger } from './creator-logger/index.js'
 export { logger } from './logger/index.js'
 export { group, groupEnd, log } from './printer/index.js'
