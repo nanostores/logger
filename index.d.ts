350ec2bd0a32ebe8c41b92044f8909de3ebd0f03
Eduard Aksamitov <e@euaaaio.ru>
2023-07-04 22:41:13 +0300

    feat: export `buildCreatorLogger`

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+export { buildCreatorLogger } from './build-creator-logger/index.js'
 export { buildLogger } from './build-logger/index.js'
 export { creatorLogger } from './creator-logger/index.js'
 export { logger } from './logger/index.js'
