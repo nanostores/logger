fffaf7e2ac12e2c793d1303f2b4c935e59d900c0
Eduard Aksamitov <e@euaaaio.ru>
2023-07-08 20:49:49 +0300

    fix: export some types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
-export { buildCreatorLogger } from './build-creator-logger/index.js'
-export { buildLogger } from './build-logger/index.js'
+export {
+  buildCreatorLogger,
+  CreatorLoggerOptions
+} from './build-creator-logger/index.js'
+export { buildLogger, LoggerOptions } from './build-logger/index.js'
 export { creatorLogger } from './creator-logger/index.js'
 export { logger } from './logger/index.js'
 export { group, groupEnd, log } from './printer/index.js'
