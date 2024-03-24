0149e60c1f7ef3014c9663fabe93c6f3153f3748
Eduard Aksamitov <e@euaaaio.ru>
2024-03-24 13:34:45 +0300

    chore: cleanup
    Signed-off-by: Eduard Aksamitov <e@euaaaio.ru>

--- logger/index.js
+++ logger/index.js
@@ -2,63 +2,10 @@ import { buildLogger } from '../build-logger/index.js'
 import { group, groupEnd, log } from '../printer/index.js'
 
 function createLogger(store, storeName, opts) {
-  // let queue = {}
-
   return buildLogger(
     store,
     storeName,
     {
-      // action: {
-      //   end: ({ actionId }) => {
-      //     for (let i of queue[actionId]) i()
-      //     delete queue[actionId]
-      //     groupEnd()
-      //   },
-
-      //   error: ({ actionId, actionName, error }) => {
-      //     queue[actionId].push(() =>
-      //       log({
-      //         message: [
-      //           ['bold', storeName],
-      //           ['regular', 'store handled error in action'],
-      //           ['bold', actionName]
-      //         ],
-      //         type: 'error',
-      //         value: {
-      //           message: error.message
-      //         }
-      //       })
-      //     )
-      //   },
-
-      //   start: ({ actionId, actionName, args }) => {
-      //     queue[actionId] = []
-
-      //     let message = [
-      //       ['bold', storeName],
-      //       ['regular', 'store was changed by action'],
-      //       ['bold', actionName]
-      //     ]
-
-      //     queue[actionId].push(() =>
-      //       group({
-      //         logo: true,
-      //         message,
-      //         type: 'action'
-      //       })
-      //     )
-      //     if (args.length > 0) {
-      //       message.push(['regular', 'with arguments'])
-      //       queue[actionId].push(() =>
-      //         log({
-      //           type: 'arguments',
-      //           value: args
-      //         })
-      //       )
-      //     }
-      //   }
-      // },
-
       change: ({ changed, newValue, oldValue, valueMessage }) => {
         let groupLog = {
           logo: true,
